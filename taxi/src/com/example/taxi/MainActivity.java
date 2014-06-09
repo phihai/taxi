9d36bbea3a91983c543f0367fcb0cf721e52f5cf
phihai <phihai.223@gmail.com>
2014-06-10 00:35:12 +0700

    First

---
+++ taxi/src/com/example/taxi/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.taxi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
